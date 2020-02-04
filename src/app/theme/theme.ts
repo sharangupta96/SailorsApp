8763314f11ffb244712c5e73e7f2362fda18fba8
Ganpathrao <sharan.ganpathrao@accenture.com>
2020-02-04 11:00:27 +0530

    UI fixes

--- src/app/theme/theme.ts
+++ src/app/theme/theme.ts
@@ -8,6 +8,7 @@ export interface Theme {
     name: "light",
     properties: {
       "--text-color": "#000000",
+      "--grey-text": "#808080",
       "--header-background-color": "#f6f6f6",
       "--listing-background-color": "#ffffff",
       "--newsletter-background-color": "#eaf4ff"
@@ -18,6 +19,7 @@ export interface Theme {
     name: "dark",
     properties: {
       "--text-color": "#fff",
+      "--grey-text": "#fff",
       "--header-background-color": "#000000",
       "--listing-background-color": "#000000",
       "--newsletter-background-color": "#007bff"
